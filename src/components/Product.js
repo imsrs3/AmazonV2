c7b986f5c5c8669b7fa7d56ab93ab64c1c3c93b6
MapleMonk user <maplemonkuser@MapleMonks-MacBook-Air.local>
2021-05-22 12:53:55 +0530

    removed key

--- src/components/Product.js
+++ src/components/Product.js
@@ -30,7 +30,7 @@ const addItemToBasket = () => {
 
 }
     return (
-        <div className="relative flex flex-col m-5 bg-white z-30 p-10" key={id}>
+        <div className="relative flex flex-col m-5 bg-white z-30 p-10 growing-hover">
             <p className="absolute top-2 right-2 text-xs italic text-gray-400">{category}</p>
 
             <Image src={image} height={200} width={200} objectFit="contain" />
@@ -41,12 +41,14 @@ const addItemToBasket = () => {
             ))}
             </div>
             <p className="text-xs my-2 line-clamp-2">{description}</p>
-            <div>
-                <Currency className="mb-5" quantity={price} currency='GBP' />
+            <div className="mb-5">
+                <Currency  quantity={price} currency='GBP' />
             </div>
             {hasPrime && (
                 <div className="flex items-center space-x-2 -mt-5">
-                   <img  className="w-12" src="https://links.papareact.com/fdw" alt="prime" />
+                   <img  
+                   loading="lazy"
+                   className="w-12" src="https://links.papareact.com/fdw" alt="prime" />
                    <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
                 </div>
             )}
